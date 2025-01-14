c34e8f027a1d8561378b07f26d43fcf23c904622
perkles <matsubaralia@gmail.com>
2025-01-14 13:57:25 -0300

    Criação do CRUD

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // configurar a timezone localmente
+  process.env.TZ = '-03:00';
+
   //habilitando globalmente a validação de dados
   app.useGlobalPipes(new ValidationPipe());
 
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,3 +1,4 @@
+import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
@@ -7,10 +8,12 @@ export class Postagem{
     @PrimaryGeneratedColumn() // INT AUTO_INCREMENT PRIMARY KEY
     id: number;
 
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty() // Validação dos dados do objeto - tem a ver com a aplicação
     @Column({length: 100, nullable: false}) // VARCHAR(100) NOT NULL - configura lá na tabela do banco de dados
     titulo: string;
 
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty()
     @Column({length: 1000, nullable: false}) // VARCHAR(1000) NOT NULL
     texto: string;
